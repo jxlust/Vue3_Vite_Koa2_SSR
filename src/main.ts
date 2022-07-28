f91522e9035bd9f593611ad4abe8f564385fcf98
xunliang jiang <xunliangjiang@xunliangdeMacBook-Pro.local>
2022-07-28 23:10:19 +0800

    feat: prettier

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@
 // import App from "./App.vue";
 // createApp(App).mount("#app");
 
-import { createSSRApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
+import { createSSRApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 export const createApp = () => {
   const app = createSSRApp(App);
   return { app };
