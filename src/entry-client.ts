e73f5e6843bb3a7b71bba697d704e60641c8213e
xunliang jiang <xunliangjiang@xunliangdeMacBook-Pro.local>
2022-07-29 23:40:16 +0800

    docs: 说明

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -12,12 +12,13 @@ app.use(pinia);
 
 // 初始化 pinia
 // 注意：__INITIAL_STATE__需要在 src/types/shims-global.d.ts中定义
+// 开发环境这个不处理也不重要
 if (window.__INITIAL_STATE__) {
-  // try {
-  //   pinia.state.value = JSON.parse(window.__INITIAL_STATE__);
-  // } catch (error) {
-  //   console.error(error);
-  // }
+  try {
+    pinia.state.value = JSON.parse(window.__INITIAL_STATE__);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 router.isReady().then(() => {
